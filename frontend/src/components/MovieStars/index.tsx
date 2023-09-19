d19c460d1b89cbb638e890f567322ea69150e887
LeandroOliveiraSouza <leandro_oliveirasouza@outlook.com>
2023-09-19 18:36:46 -0300

    MovieCard

---
+++ frontend/src/components/MovieStars/index.tsx
@@ -0,0 +1,16 @@
+import "./styles.css";
+import { ReactComponent as StarFull } from "assets/star-full.svg";
+import { ReactComponent as StarHalf } from "assets/star-half.svg";
+import { ReactComponent as StarEmpty } from "assets/star-empty.svg";
+
+export default function MovieStars() {
+  return (
+    <div className="dsmovie-stars-container">
+      <StarFull />
+      <StarFull />
+      <StarFull />
+      <StarHalf />
+      <StarEmpty />
+    </div>
+  );
+}
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,9 +1,32 @@
+import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
 
 export default function Listing() {
   return (
     <>
       <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
